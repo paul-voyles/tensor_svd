e5123c137b91fbe1fb3b3b0be40c66cb1779d904
Chenyu Zhang <czhang376@wisc.edu>
2020-04-16 12:50:08 -0500

    Unfold_axis function finished

--- tensor_svd/unfold_axis.py
+++ tensor_svd/unfold_axis.py
@@ -1,11 +1,21 @@
 import numpy as np
 
-dim = 1
-t = np.linspace(1,1000,1000)
-t = np.reshape(t,[10,10,10])
-print(t[0,:,0])
-# t1 = np.transpose(t,(0,2,1))
-t1 = np.reshape(t,[10,100])
-# print(t1[2,:])
-# print(t[0,:,:])
-# print(t[:,0,:])
+def unfold_axis(data, k):
+    # def unfold_axis(data, target_dim):
+    data = np.linspace(1,10000,10000)
+    data = np.reshape(data,[10,10,10,10])
+    # print(t[0,:,0,0])
+
+    target_dim = k
+    total_dim = len(data.shape)
+
+    dim_list = []
+    for i in range(total_dim):
+        dim_list.append((target_dim - i) % total_dim)
+    dim_order = tuple(dim_list)
+    # print(dim_order)
+
+    data_unfold = np.transpose(data,dim_order)
+    data_unfold = np.reshape(data_unfold,[data.shape[0],int(data.size/data.shape[0])])
+    # print(data_unfold[2,:])
+    return data_unfold
